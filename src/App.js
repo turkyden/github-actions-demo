42d3301a870d5c509e5368d4e1a6a4036bdd6b7b
wj871287@gmail.com <wj871287@gmail.com>
2019-12-26 17:35:20 +0800

    Fixed the app

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Awesome React
         </a>
       </header>
     </div>
